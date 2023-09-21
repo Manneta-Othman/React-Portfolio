e2eb6533bba0477d672c9e5275891930c169531f
Manneta Othman <othman.manneta@gmail.com>
2023-09-21 18:18:00 +0100

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route index path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='*' element={<Default />} />
