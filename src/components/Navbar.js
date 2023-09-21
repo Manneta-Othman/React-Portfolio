c227e36902e880bdf09d127bd54b67ac493d5579
Manneta Othman <othman.manneta@gmail.com>
2023-09-21 18:20:02 +0100

    fixing links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 import {BsGithub, BsLinkedin} from 'react-icons/bs';
 import {AiOutlineMenu} from 'react-icons/ai';
@@ -15,15 +16,15 @@ export const Navbar = () => {
             <AiOutlineMenu id='menu-icon' onClick={() => setShow(prev => !prev)}/>
 
             <ul className={show ? 'nav-links active' : 'nav-links'}>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/#skills">Teck Stack</a></li>
-                <li><a href="/#projects">Projects</a></li>
-                <li><a href="/contact">Contact</a></li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/#skills">Teck Stack</Link></li>
+                <li><Link to="/#projects">Projects</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
 
                 <div className="nav-icons">
-                    <a href="https://github.com/Manneta-Othman" target='_blank' rel="noreferrer" className="github"> <BsGithub className='icon' /> </a>
-                    <a href="https://www.linkedin.com/in/othman-manneta-504293204/" rel="noreferrer" target='_blank' className="linkedin"> <BsLinkedin className='icon' /> </a>
+                    <Link to="https://github.com/Manneta-Othman" target='_blank' rel="noreferrer" className="github"> <BsGithub className='icon' /> </Link>
+                    <Link to="https://www.linkedin.com/in/othman-manneta-504293204/" rel="noreferrer" target='_blank' className="linkedin"> <BsLinkedin className='icon' /> </Link>
                 </div>
             </ul>
 
