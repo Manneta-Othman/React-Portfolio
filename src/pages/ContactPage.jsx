5c5947c0dd976aac998bd938af65e1f0e4070d77
Othman Manneta <othman.manneta@gmail.com>
2023-11-07 14:55:29 +0100

    lost count of changes num

---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Heading } from '../components/Heading';
+
+
+export function Contact() {
+  return (
+    <section className='contact-page'>
+      <>
+        <Heading title='For any querie, please email me on:' subtitle='Othman.manneta@gmail.com' />
+      </>
+    </section>
+  )
+}
