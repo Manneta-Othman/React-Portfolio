15a6d1ec59f171117b9beb365d7ec2a982dc72d4
Othman Manneta <othman.manneta@gmail.com>
2023-11-08 05:39:57 +0100

    lost count of changes num

---
+++ src/components/card.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import './card.css'
+
+
+export function Card({ tech, setIsPopupOpen }) {
+
+  const [isCardOpen, setIsCardOpen] = useState([])
+
+
+  function dropDown(name) {
+    isCardOpen.includes(name) ? setIsCardOpen('') : setIsCardOpen(name)
+  }
+
+  function handlePopUpProjects() {
+    setIsPopupOpen(tech.id)
+  }
+
+
+  return <div className="card">
+    <div className="top" onClick={() => dropDown(tech.lg)}>
+      {
+        typeof tech.icon === 'object' ?
+          <>
+            <div className="image">
+              <img src={tech.icon[0]} alt="html" />
+              <h1>HTML</h1>
+            </div>
+            <div className="image">
+              <img src={tech.icon[1]} alt="css" />
+              <h1>CSS</h1>
+            </div>
+          </>
+          :
+          <div className="image">
+            <img src={tech.icon} alt="html" />
+            <h1>{tech.lg}</h1>
+          </div>
+      }
+
+    </div>
+    <div className={isCardOpen === tech.lg ? "bottom open" : "bottom"}>
+      <div className="desc">
+        <h2>Description</h2>
+        <p>{tech.desc}</p>
+      </div>
+      <div className="proj">
+        <h2>Projects</h2>
+        <div className="single-project">
+          <p>
+            {tech.projects.length > 0 ?
+              <>
+                {tech.statement} <span onClick={handlePopUpProjects}> Clicking Here</span>
+              </> 
+              :
+              <>
+                {tech.statement}
+
+                { tech.link && <a href={tech.link} rel="noreferrer" target='_blank' > Clicking Here</a>
+                }
+              </>}
+          </p>
+        </div>
+      </div>
+      <div className="certification">Certification</div>
+    </div>
+  </div>;
+}
---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Heading } from '../components/Heading';
+
+
+export function Contact() {
+  return (
+    <section className='contact-page'>
+      <>
+        <Heading title='For any querie, please email me on:' subtitle='Othman.manneta@gmail.com' />
+        <a href="mailto:othman.manneta@gmail.com">Manneta Othman</a>
+
+      </>
+    </section>
+  )
+}
