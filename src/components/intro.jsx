4effa57b48182798455ff78dfc51258fc120ad98
Othman Manneta <othman.manneta@gmail.com>
2023-11-10 13:03:46 +0100

    adding some css final touches

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -3,17 +3,6 @@ import React from "react";
 
 export function Intro({ text }) {
 
-  const style = {
-    maxWidth: "700px",
-    color: "#666",
-    fontFamily: 'Poppins',
-    fontSize: "20px",
-    fontStyle: "normal",
-    fontWeight: "400",
-    textAlign: "justify",
-    marginTop: "20px"
-  }
-
   return (
     <p
       className="intro"
